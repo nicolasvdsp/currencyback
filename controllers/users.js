dd7e6225b81037a79f34957cf0762f2d83f9f012
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-04-24 21:30:20 +0200

    feat: added Scheme model for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+});
+const User = mongoose.model('User', userSchema);
+
 const getAll = (req, res, next) => {
   res.send("respond with a resource");
 };
 
 const create = (req, res, next) => {
-  res.send("respond with a resource");
+  let user = new User();
+    user.name = "John";
+    user.email = "email";
+    user.password = "password";
+    user.save((err, doc) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(
+            {
+                "status": 'User created!',
+                "data": {
+                    "name": doc,
+                }
+            }
+        );
+    });
 };
+    
 
 module.exports = {
   getAll,
