9f91bcc466a643fa5e12ab07ddc809a15a38a525
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-04-24 21:29:23 +0200

    build: added mongoose

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/currencyapp', { useNewUrlParser: true });
+
 var app = express();
 
 // view engine setup
