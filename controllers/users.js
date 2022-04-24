a2382c617227010e285195407045630abc7eb40e
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-04-24 20:52:12 +0200

    feat: added controllers to add more structure

---
+++ controllers/users.js
@@ -0,0 +1,12 @@
+const getAll = (req, res, next) => {
+  res.send("respond with a resource");
+};
+
+const create = (req, res, next) => {
+  res.send("respond with a resource");
+};
+
+module.exports = {
+  getAll,
+  create,
+};
