8d004e90503cb4d421e494ded092498acf262099
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-04-24 22:12:09 +0200

    refactor: changed userSchema

--- models/users.js
+++ models/users.js
@@ -1,21 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
+const User = new Schema({});
+User.plugin(passportLocalMongoose);
 
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-});
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', User);
