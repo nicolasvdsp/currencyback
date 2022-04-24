70cfcf5c624a495e9b38e28211995ba24f5ae893
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-04-24 20:47:50 +0200

    feat: bareboned post route added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.json({
+    "status": "success",
+    "message": "This is the users route",
+    "data": [],
+  })
 });
+
+//make post request to /users
+router.post('/', (req, res, next) => {
+  res.json({
+    "status": "success",
+    "message": "This is the users route",
+    "data": [],
+  })
+});
+
 
 module.exports = router;
