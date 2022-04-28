907439191101789985543e97259e3e6ceac951f5
ismailElg <91196906+ismailElg1@users.noreply.github.com>
2022-04-28 12:36:39 +0200

    refactor: changed the structure of the user

--- models/users.js
+++ models/users.js
@@ -1,8 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
 
-const User = new Schema({});
-User.plugin(passportLocalMongoose);
+//create user schema
+const User = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    balance: {
+        type: Number,
+        default: 0,
+    },
+    
+
+});
+
 
 module.exports = mongoose.model('User', User);
