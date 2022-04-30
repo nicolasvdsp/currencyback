319b02cdb979ee4072eb3df5c00197f5acd588d4
Nicolas van der Straten Ponthoz <nicolas@Nicolas-MacBook-Pro.local>
2022-04-30 21:25:35 +0200

    feat(transactions): get data from and post data to mongodb

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,24 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const transactionSchema = new Schema({
+    sender: String,
+    receiver: String,
+    amount: Number,
+    date: {type: Date, default: Date.now }
+})
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
 const getAll = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "transactions": []
+    Transaction.find((err, docs) => {
+        if(!err){
+            res.json({
+                "status": "success",
+                "data": {
+                    "transactions": docs
+                }
+            });
         }
-    });
-}
+    })
+}    
 
 const create = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "transaction": {
-                "sender": "Aiden",
-                "receiver": "Jesse",
-                "amount": "420",
-                "date": "25/12/2020"
-            }
+    let transaction = new Transaction();
+    transaction.sender = "Aiden";
+    transaction.receiver = "Jesse";
+    transaction.amount = 3;
+    transaction.save((err, doc) => {
+        if(!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "transaction": doc
+                }
+            });
         }
-    });
+    })
 }
 
 module.exports = {
