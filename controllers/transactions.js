f251dbfac24d7babb3d35a1ccd947eea02c03ba7
Nicolas van der Straten Ponthoz <nicolas@laptop-v4rkgalo.eduroam.thomasmore.be>
2022-05-12 09:53:45 +0200

    feat(transactions): check if user has enough balance before making the transaction

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -22,53 +22,47 @@ const getAll = (req, res) => {
 
 const create = async (req, res) => {
     try {
-        let transaction = new Transaction();
-        transaction.sender = req.body.sender;
-        transaction.receiver = req.body.receiver;
-        transaction.amount = req.body.amount;
+        const amount = req.body.amount;
+        const sender = await User.find(
+            { username: req.body.sender}, 'balance'
+        );
+        const senderBalance = sender[0].balance;
         
-        const transactionSaved = await transaction.save();
+        if(senderBalance >= amount) {
+            let transaction = new Transaction();
+            transaction.sender = req.body.sender;
+            transaction.receiver = req.body.receiver;
+            transaction.amount = req.body.amount;
+        
+            const transactionSaved = await transaction.save();
 
-        await User.updateOne(
-            { username: transaction.receiver },
-            { $inc: {balance: (transaction.amount)} }
-        );
-        await User.updateOne(
-            { username: transaction.sender },
-            { $inc: {balance: -(transaction.amount)} }
-        );
+            await User.updateOne(
+                { username: transaction.receiver },
+                { $inc: {balance: (transaction.amount)} }
+            );
+            await User.updateOne(
+                { username: transaction.sender },
+                { $inc: {balance: -(transaction.amount)} }
+            );
 
-        res.json({
-            "status": "success",
-            "data": {
-                "transaction": transactionSaved
-            }
-        });
-
+            res.json({
+                "status": "success",
+                "data": {
+                    "transaction": transactionSaved
+                }
+            });
+        } else {
+            res.json({
+                "status": "error",
+                "message": "You're to broke for this man"
+            })
+        }
     } catch {
         res.json({
             "status": "error",
             "message": "Could not make the transaction"
         });
     }
-}
-
-//updateBalanceSender niet als aparte functie, (te beel php achtige code)
-//eerst met callbacks werken, nadien eventueel refactoren naar promises (await async)
-//increment kan je rechtstreeks in een User.update({username: sender} {balance $inc{....}}) iets in die aard doen
-
-let currentBalance;
-const updateBalanceSender = (sender, amount) => {
-    let newBalance;
-    User.find({username: sender}, (err, user) => {
-        User.update
-        currentBalance = user[0].balance;
-        newBalance = currentBalance + amount;
-        console.log(`${currentBalance} becomes ${newBalance}`);
-        user.balance = newBalance;
-        console.log(user);
-        res.send
-    });
 }
 
 module.exports = {
