a628fc3216f71c58bc75bb7285a9b4a7a9182710
Nicolas van der Straten Ponthoz <nicolas@desktop-06onpmv.guests.thomasmore.be>
2022-05-19 10:46:11 +0200

    feat: add cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/currencyapp', { useNewUrlParser: true });
 
 var app = express();
+const cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
