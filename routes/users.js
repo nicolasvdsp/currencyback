ec7cbb285b1dd0291306937d0571a6b441cc00ef
Nicolas van der Straten Ponthoz <nicolas@desktop-ol8uker.eduroam.thomasmore.be>
2022-05-19 19:48:38 +0200

    fix: restore auth

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
-// const authController = require('../controllers/auth');
+const authController = require('../controllers/auth');
 
 /* GET users listing. */
 router.get('/', userController.getAll);
 router.get('/getallbybalance', userController.getAllByBalance);
 
-// router.post('/signup', authController.signup);
-// router.post('/login', authController.login);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 module.exports = router;
